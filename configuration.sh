cf28e713dfd22f36971dcf45cdec1dba9d31a6c2
Faraz Ahmad <l1f21bsse0147@ucp.edu.pk>
2025-01-20 09:02:33 +0500

    forst commit

---
+++ configuration.sh
@@ -0,0 +1,8 @@
+echo "Repo Intial configuration"
+git branch -m "develop"
+git config --local user.name "Faraz Ahmad"
+git config --local user.email l1f21bsse0147@ucp.edu.pk
+git config --local core.editor
+mkdir ./src ./style
+touch ./src/index.html ./styles/index.css .gitignore
+npm init -y
